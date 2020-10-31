ee6074206f564ec4fb39442f26f79bfe70b8cf03
Michael Irvine <michael.j.irvine@gmail.com>
2020-10-31 10:37:30 -0400

    read from earliest to latest so we get the most recent set of file for each day

--- processing/parse_data.sh
+++ processing/parse_data.sh
@@ -2,7 +2,7 @@
 
 awk 'BEGIN{FS=OFS=","}{print "date" OFS $0}' <(head -1 ../coronavirus-data/data-by-modzcta.csv) > output.csv
 cd ../coronavirus-data
-git log $1 --pretty=format:"%h" | while read line ; do
+git log $1 --pretty=format:"%h" | tac | while read line ; do
 	git checkout $line
 	date=$(git log -1 --pretty=format:"%cd" --date=short)
 	awk -v date="$date" 'BEGIN{FS=OFS=","}{print date OFS $0}' <(tail -n +2 data-by-modzcta.csv) > ../processing/output_"$date".csv
